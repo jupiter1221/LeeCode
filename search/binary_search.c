9f982e45796d65e10f122f47c172ae615ba991dd
jupiter_chen <jupiter_chen@asus.com>
2017-07-18 14:34:57 +0800

    [Search]Binary search

---
+++ search/binary_search.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int binary_seach(const int sort_list[],int array_size,int target);
+
+int main(void) {
+	int a[] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+	binary_seach(a, sizeof(a)/sizeof(int),6);
+	return 1;
+}
+
+int binary_seach(const int sort_list[],int array_size,int target)
+{
+	int middle = 0;
+	int left = 0;
+	//int b[] = {1,2,3,4,5,6,7,8,9};
+	int right = array_size;
+	//int bszie = sizeof(b);
+	//printf("bsize = %d\n",bszie);
+
+	printf("Start binary_search() !\n");
+	printf("----right = %2d, left = %d---- \n",right, left);
+	while (left <= right) {
+		middle = (right + left) / 2 ;
+		printf("right = %2d, left = %2d,,middle  = %2d,sort_list[middle] = %2d \n",right, left,middle,sort_list[middle]);
+		if(sort_list[middle] == target) {
+			printf("Find !!! %d \n",middle);
+			return middle;
+ 		}
+		else if (sort_list[middle] > target)
+			right = middle - 1;
+		else 
+			left = middle + 1;	
+			
+	}
+	printf("Can not find !\n");
+}
