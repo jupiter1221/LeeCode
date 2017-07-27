41ce62966294b7f94c79b63c1ba39ddd28234610
jupiter_chen <jupiter_chen@asus.com>
2017-07-27 17:45:09 +0800

    [swap] swap
    1.normal swap use tmp
    2.subtract
    3.xor

---
+++ swap/swap_xor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int swap_xor(int *a, int *b);
+
+int main(void) {
+	int *a = malloc(sizeof(int));
+	int *b = malloc(sizeof(int));
+	*a = -10;
+	*b = 27;
+	swap_xor(a, b);
+
+clean:
+	free(a);
+	free(b);
+	return 1;
+}
+
+int swap_xor(int *a, int *b)
+{
+	printf("Befor swap a=%2d, b=%2d \n",*a,*b);
+	*a=*a ^ *b;
+	*b=*a ^ *b;
+	*a=*a ^ *b;
+	printf("After swap a=%2d, b=%2d \n",*a,*b);
+}
