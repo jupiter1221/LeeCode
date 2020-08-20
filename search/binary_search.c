6cf17581ed8e47641537d984ab6fcbf391e01e08
jupiter1221 <jupiter_chen@asus.com>
2020-08-20 13:40:07 +0800

    [fix] add print binary sort data list

--- search/binary_search.c
+++ search/binary_search.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+#define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
+
 int binary_seach(const int sort_list[],int array_size,int target);
 
 int main(void) {
 	int a[] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+    printf("Data list:\n");
+    for(int i=0;i< NELEMS(a);i++)
+        printf(" %2d ",a[i]);
+    printf("\n");
 	binary_seach(a, sizeof(a)/sizeof(int),6);
 	return 1;
 }
