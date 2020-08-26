f3dc0fdf617cde52d694c2dfd34d5675d51bc09a
jupiter1221 <jupiter_chen@asus.com>
2020-08-26 13:55:33 +0800

    [temp]

--- sort/bubble_sort.c
+++ sort/bubble_sort.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define array_size(x) sizeof(x)/sizeof(x[0])
+#define array_size(x)   ((sizeof(x))/(sizeof(x[0])))
 
 void print_array(int list[],int array_size);
 
@@ -18,21 +18,28 @@ void swap (int *a, int *b) {
 
 int bubble_sort (int data[]) {
     int min_idx = 0;
-    size = array_size(data);
-
+    int size = array_size(data);
+    printf("size = %d\n",size);
     for (int j = 0; j < size; j++) {
         print_array(data,sizeof(data)/sizeof(data[0]));
         for (int i = j + 1;i < size;i++) {
             if (data[i] > data[i-1])
                 swap(&data[i], &data[i-1]);
         }
+        //print_array(data,sizeof(data)/sizeof(data[0]));
     }
-
 
 }
 int main(void) {
     int data[]= {1,3,3,1,2,10,8,4,11,20,9,12,13};
-    print_array(data,sizeof(data)/sizeof(data[0]));
-    swap(&data[3], &data[4]);
-    print_array(data, sizeof(data)/sizeof(data[0]));
+    int dd = array_size(data);
+    //printf(" %2d",sizeof(data));
+    //printf(" %2d",sizeof(data[0]));
+
+    //printf(" %2d ",dd);
+    //printf("\n");
+    //print_array(data,sizeof(data)/sizeof(data[0]));
+    //swap(&data[3], &data[4]);
+    //print_array(data, sizeof(data)/sizeof(data[0]));
+    bubble_sort(data);
 }
